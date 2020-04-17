b101f01fc287a40fcb52d11fd3f5bc59a5924529
Josh Tummel <josh@astigmapro.com>
2020-04-16 20:34:34 -0400

    rework

---
+++ precache-manifest.6f4b44dc8fcb598f3a99b724a78889c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aeeff1e263004aa7f53078e26938731",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fa5c92283c214ad38df",
+    "url": "/static/js/2.7a23b3a8.chunk.js"
+  },
+  {
+    "revision": "8b14456ca0045207736ab113c9f6da4d",
+    "url": "/static/js/2.7a23b3a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89820a10d5d2bb95820d",
+    "url": "/static/js/main.8c7d2809.chunk.js"
+  },
+  {
+    "revision": "042b4c265205a503bbcf",
+    "url": "/static/js/runtime-main.51c80cb1.js"
+  }
+]);
