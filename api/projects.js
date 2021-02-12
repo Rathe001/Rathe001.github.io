ea84fc4a9decc3180612abeb8a45126d03eb7c23
Josh Tummel <jtummel@bottomline.com>
2021-02-12 08:51:02 -0500

    Working on CORS

--- api/projects.js
+++ api/projects.js
@@ -1,11 +1,12 @@
 import { readdirSync, lstatSync } from 'fs';
 import path from 'path';
+import cors from 'cors';
 
 const getDirectories = (srcPath) => readdirSync(srcPath)
   .filter((file) => lstatSync(path.join(srcPath, file)).isDirectory());
 
 export default (app) => {
-  app.get('/projects', (req, res) => {
+  app.get('/projects', cors(), (req, res) => {
     res.json(getDirectories('./public/projects'));
   });
 };
--- api/server.js
+++ api/server.js
@@ -1,12 +1,10 @@
 import express from 'express';
-import cors from 'cors';
 import projects from './projects';
 
 const app = express();
 
 projects(app);
 
-app.use(cors());
 app.listen(process.env.PORT || 3001, () => {
   console.log(`Server running on port ${process.env.PORT || 3001}`);
 });
